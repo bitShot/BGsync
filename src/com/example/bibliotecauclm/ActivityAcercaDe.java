e127e4d0a37bb47c04f43a18415219c6d0d0d2f6
bitShot <crespo.sonseca@gmail.com>
2014-04-13 12:00:27 +0200

    Commit desde eclipse

--- src/com/example/bibliotecauclm/ActivityAcercaDe.java
+++ src/com/example/bibliotecauclm/ActivityAcercaDe.java
@@ -1,5 +1,5 @@
 /*
-    This file is part of BGsync.
+    This file is part of BGsync .
 
     BGsync is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
