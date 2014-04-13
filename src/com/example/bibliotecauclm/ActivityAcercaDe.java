61a0bbcfde2d836de0e44fe0cbe8738357e7f135
bitShot <crespo.sonseca@gmail.com>
2014-04-13 14:34:19 +0200

    Test

--- src/com/example/bibliotecauclm/ActivityAcercaDe.java
+++ src/com/example/bibliotecauclm/ActivityAcercaDe.java
@@ -1,5 +1,5 @@
 /*
-    This file is part of BGsync .
+    This file is part of BGsync.
 
     BGsync is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -8,7 +8,7 @@
 
     BGsync is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
